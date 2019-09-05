aa61c8f691fec59fea7970d83886aca2e4dce65b
darwinner3 <darwinner3@163.com>
2019-09-05 23:43:17 +0800

    整合本地资源

---
+++ learnPy.py
@@ -0,0 +1,23 @@
+for  i in range(2,5):
+    print(i)            
+    
+
+print("/home/darwin/Desktop/PythonVSCode/learnPy.py ")
+print("/home/darwin/Desktop/PythonVSCode/learnPy.py ")
+
+
+apcd = 5+3
+apcd = 8-0
+print(apcd)
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print(i)
+    
